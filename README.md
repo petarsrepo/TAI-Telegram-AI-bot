fb42300d2d893b104e2a53de1972ffb483f7f163
PeteStoik <petar.i.stoykov@gmail.com>
2023-04-01 09:20:20 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# TAI Telegram AI Bot
+
+TAI AI is back! Smarter, better, faster, stronger,  powered by the OpenAI API. Ask your question and find the answer.
+
+## Prerequisites
+
+* Node.js: ^16.17.0— [install Node](https://nodejs.org/en/download/)
+
+## Install
+
+To install this project:
+
+Clone this repository:
+
+```sh
+git clone https://github.com/petarsrepo/TAI-Telegram-AI-bot.git
+```
+
+Install dependencies:
+
+From the root directory of the project run:
+
+```sh
+npm ci
+```
+
+> Use `npm ci` to launch a `clean install` of the dependencies, this will install the same version as in the `package.json` file.
+
+
+## Usage
+
+Type `/query YOUR_QUESTION` to receive an answer to your query.
---
+++ index.js
@@ -0,0 +1,57 @@
+  require('dotenv').config()
+  const express = require('express');
+  const app = express();
+  const path = require('path');
+  const {
+    Configuration,
+    OpenAIApi
+  } = require("openai");
+  const TelegramBot = require('node-telegram-bot-api');
+  const token = process.env.BOT_TOKEN
+  const bot = new TelegramBot(token, {
+    polling: true
+  });
+  const fs = require('fs');
+
+  bot.onText(/\/query (.+)/, (msg, body) => {
+
+    async function getReply(query) {
+      const chatId = msg.chat.id;
+      const resp = await getInput(body)
+      bot.sendMessage(chatId, resp);
+    }
+
+    getReply(body)
+
+  });
+
+  async function getInput(query) {
+    const response = await runPrompt(query);
+    console.log(response);
+    return response;
+  }
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  async function runPrompt(prompt) {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ["\"\"\""],
+    });
+    console.log('OpenAI API response:', response);
+    return response.data.choices[0].text;
+  }
